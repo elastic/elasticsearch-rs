164e6674d96324505932cb31f9ef1a65f2ef1f6d
Russ Cam <russ.cam@elastic.co>
2019-11-05 08:26:05 +1000

    Return Result<T,E> types from tests

--- elasticsearch/src/lib.rs
+++ elasticsearch/src/lib.rs
@@ -32,11 +32,12 @@ pub mod tests {
     use reqwest::StatusCode;
     use serde_json::Value;
     use url::Url;
+    use failure;
 
     #[test]
-    fn test_search_with_body() {
-        let client = Elasticsearch::default();
+    fn test_search_with_body() -> Result<(), failure::Error> {
 
+        let client = Elasticsearch::default();
         let mut response = client
             .search::<Value>()
             .body(Some(json!({
@@ -45,31 +46,32 @@ pub mod tests {
                 }
             })))
             .allow_no_indices(Some(true))
-            .send()
-            .unwrap();
+            .send()?;
 
         assert_eq!(response.status_code(), StatusCode::OK);
-
-        let response_body = response.read_body::<Value>().unwrap();
-
+        let response_body = response.read_body::<Value>()?;
         assert!(response_body["took"].as_i64().unwrap() > 0);
+
+        Ok(())
     }
 
     #[test]
-    fn test_search_no_body() {
+    fn test_search_no_body() -> Result<(), failure::Error> {
         let client = Elasticsearch::default();
-
         let mut response = client
             .search::<()>()
             .pretty(Some(true))
             .q(Some("title:Elasticsearch".into()))
-            .send()
-            .unwrap();
+            .send()?;
 
         assert_eq!(response.status_code(), StatusCode::OK);
-
-        let response_body = response.read_body::<Value>().unwrap();
-
+        let response_body = response.read_body::<Value>()?;
         assert!(response_body["took"].as_i64().unwrap() > 0);
+
+        for hit in response_body["hits"]["hits"].as_array().unwrap() {
+            assert!(hit["_source"]["title"].as_str().is_some());
+        }
+
+        Ok(())
     }
 }
