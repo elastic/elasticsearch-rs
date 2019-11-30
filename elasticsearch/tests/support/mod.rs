fc71863ec0233dbbfddb14a977029846979b9e7c
Russ Cam <russ.cam@elastic.co>
2019-11-30 17:24:55 +1000

    Add user agent version based on cargo package version (#26)
    This commit adds a default user-agent header to the client of the
    form
    
    elasticsearch-rs/{version}
    
    where version is the cargo package version. This format matches
    the agreed format for official language clients.
    
    Update the package versions for the client and api_generator to
    match that of the REST API spec version from which the client is
    generated. The client versioning will follow at least the
    major/minor of Elasticsearch, similar to other official clients.

---
+++ elasticsearch/tests/support/mod.rs
@@ -0,0 +1,9 @@
+// From reqwest crate
+// Licensed under Apache License, Version 2.0
+// https://github.com/seanmonstar/reqwest/blob/master/LICENSE-APACHE
+
+pub mod server;
+
+#[allow(unused)]
+pub static DEFAULT_USER_AGENT: &'static str =
+    concat!("elasticsearch-rs/", env!("CARGO_PKG_VERSION"));
