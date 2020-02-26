72ef844cae4b9e443cc0da2774f00217c1a389f8
Russ Cam <russ.cam@elastic.co>
2020-02-26 23:33:46 +1000

    Fix run-elasticsearch.ps1 script
    This commit fixes the PowerShell script to configure TLS certificates when
    running the X-Pack suite.
    
    Generate new certificates, including the CA private key, so that other
    certificates can be generated in future.

--- .ci/run-elasticsearch.ps1
+++ .ci/run-elasticsearch.ps1
@@ -4,38 +4,38 @@
 # Export the ELASTICSEARCH_VERSION variable, eg. 'elasticsearch:8.0.0-SNAPSHOT'.
 
 param(
-    [string]
-    $NODE_NAME,
+  [string]
+  $NODE_NAME,
 
-    [string]
-    $MASTER_NODE_NAME,
+  [string]
+  $MASTER_NODE_NAME,
 
-    [string]
-    $CLUSTER_NAME,
+  [string]
+  $CLUSTER_NAME,
 
-    [int]
-    $HTTP_PORT = 9200,
+  [int]
+  $HTTP_PORT = 9200,
 
-    [string]
-    $ELASTIC_PASSWORD = "changeme",
+  [string]
+  $ELASTIC_PASSWORD = "changeme",
 
-    [string]
-    $SSL_CERT = "$PWD/certs/testnode.crt",
+  [string]
+  $SSL_CERT = "$($(Split-Path $script:MyInvocation.MyCommand.Path).Replace("\", "/"))/certs/testnode_san.crt",
 
-    [string]
-    $SSL_KEY = "$PWD/certs/testnode.key",
+  [string]
+  $SSL_KEY = "$($(Split-Path $script:MyInvocation.MyCommand.Path).Replace("\", "/"))/certs/testnode_san.key",
 
-    [string]
-    $SSL_CA = "$PWD/certs/ca.crt",
+  [string]
+  $SSL_CA = "$($(Split-Path $script:MyInvocation.MyCommand.Path).Replace("\", "/"))/certs/ca.crt",
 
-    [switch]
-    $DETACH,
+  [switch]
+  $DETACH,
 
-    [switch]
-    $CLEANUP,
+  [switch]
+  $CLEANUP,
 
-    [string]
-    $NETWORK_NAME
+  [string]
+  $NETWORK_NAME
 
 )
 
@@ -50,7 +50,7 @@ if ($null -eq $env:ELASTICSEARCH_VERSION) {
   exit 1
 }
 
-$moniker = $env:ELASTICSEARCH_VERSION -replace "[^a-zA-Z\d]", "-" 
+$moniker = $env:ELASTICSEARCH_VERSION -replace "[^a-zA-Z\d]", "-"
 $suffix = "rest-test"
 
 if (!$NODE_NAME) {
@@ -83,6 +83,23 @@ function cleanup_volume {
     docker volume rm "$Name"
   }
 }
+
+function container_running {
+  param(
+    [string]
+    $Name
+  )
+
+  $output = "$(docker ps --quiet --filter name="$Name")"
+  Write-Output "container_running output $output"
+
+  if ($output) {
+    return $true
+  } else {
+    return $false
+  }
+}
+
 function cleanup_node {
   param(
     [string]
@@ -129,7 +146,7 @@ if ($CLEANUP) {
     Write-Output "$ESC[34;1mINFO:$ESC[0m $NETWORK_NAME is already deleted$ESC[0m"
     exit 0
   }
-  $containers = $(docker network inspect --format '{{ range $key, $value := .Containers }}{{ printf "%s\n" .Name}}{{ end }}' ${NETWORK_NAME})
+  $containers = $(docker network inspect --format '{{ range $key, $value := .Containers }}{{ println .Name}}{{ end }}' ${NETWORK_NAME})
 
   foreach($container in $containers) {
     cleanup_node "$container"
@@ -147,51 +164,56 @@ Write-Output "$ESC[34;1mINFO:$ESC[0m Creating network $NETWORK_NAME if it does n
 
 docker network inspect "$NETWORK_NAME" | Out-Null
 if ($LASTEXITCODE -ne 0) {
-  docker network create "$NETWORK_NAME" 
+  docker network create "$NETWORK_NAME"
 }
 
 $environment = @(
-  "--env", "node.name=`"$NODE_NAME`"",
-  "--env", "cluster.name=`"$CLUSTER_NAME`"",
-  "--env", "cluster.initial_master_nodes=`"$MASTER_NODE_NAME`"",
-  "--env", "discovery.seed_hosts=`"$MASTER_NODE_NAME`"",
-  "--env", "cluster.routing.allocation.disk.threshold_enabled=false",
-  "--env", "bootstrap.memory_lock=true",
-  "--env", "node.attr.testattr=test",
-  "--env", "path.repo=/tmp",
-  "--env", "repositories.url.allowed_urls=http://snapshot.test*"
+"--env", "node.name=`"$NODE_NAME`"",
+"--env", "cluster.name=`"$CLUSTER_NAME`"",
+"--env", "cluster.initial_master_nodes=`"$MASTER_NODE_NAME`"",
+"--env", "discovery.seed_hosts=`"$MASTER_NODE_NAME`"",
+"--env", "cluster.routing.allocation.disk.threshold_enabled=false",
+"--env", "bootstrap.memory_lock=true",
+"--env", "node.attr.testattr=test",
+"--env", "path.repo=/tmp",
+"--env", "repositories.url.allowed_urls=http://snapshot.test*"
 )
 
 $volumes = @(
-  "--volume", "${volume_name}:/usr/share/elasticsearch/data"
+"--volume", "${volume_name}:/usr/share/elasticsearch/data"
 )
 
-if (-not $env:ELASTICSEARCH_VERSION -contains "oss") {
+if (-not ($env:ELASTICSEARCH_VERSION -contains "oss")) {
   $environment += @(
-    "--env", "ELASTIC_PASSWORD=`"$ELASTIC_PASSWORD`"",
-    "--env", "xpack.license.self_generated.type=trial",
-    "--env", "xpack.security.enabled=true",
-    "--env", "xpack.security.http.ssl.enabled=true",
-    "--env", "xpack.security.http.ssl.verification_mode=certificate",
-    "--env", "xpack.security.http.ssl.key=certs/testnode.key",
-    "--env", "xpack.security.http.ssl.certificate=certs/testnode.crt",
-    "--env", "xpack.security.http.ssl.certificate_authorities=certs/ca.crt",
-    "--env", "xpack.security.transport.ssl.enabled=true",
-    "--env", "xpack.security.transport.ssl.key=certs/testnode.key",
-    "--env", "xpack.security.transport.ssl.certificate=certs/testnode.crt",
-    "--env", "xpack.security.transport.ssl.certificate_authorities=certs/ca.crt"
+  "--env", "ELASTIC_PASSWORD=`"$ELASTIC_PASSWORD`"",
+  "--env", "xpack.license.self_generated.type=trial",
+  "--env", "xpack.security.enabled=true",
+  "--env", "xpack.security.http.ssl.enabled=true",
+  "--env", "xpack.security.http.ssl.verification_mode=certificate",
+  "--env", "xpack.security.http.ssl.key=certs/testnode.key",
+  "--env", "xpack.security.http.ssl.certificate=certs/testnode.crt",
+  "--env", "xpack.security.http.ssl.certificate_authorities=certs/ca.crt",
+  "--env", "xpack.security.transport.ssl.enabled=true",
+  "--env", "xpack.security.transport.ssl.key=certs/testnode.key",
+  "--env", "xpack.security.transport.ssl.certificate=certs/testnode.crt",
+  "--env", "xpack.security.transport.ssl.certificate_authorities=certs/ca.crt"
   )
 
   $volumes += @(
-    "--volume", "`"${SSL_CERT}`":/usr/share/elasticsearch/config/certs/testnode.crt",
-    "--volume", "`"${SSL_KEY}`":/usr/share/elasticsearch/config/certs/testnode.key",
-    "--volume", "`"${SSL_CA}`":/usr/share/elasticsearch/config/certs/ca.crt"
+  "--volume", "`"${SSL_CERT}:/usr/share/elasticsearch/config/certs/testnode.crt`"",
+  "--volume", "`"${SSL_KEY}:/usr/share/elasticsearch/config/certs/testnode.key`"",
+  "--volume", "`"${SSL_CA}:/usr/share/elasticsearch/config/certs/ca.crt`""
   )
 }
 
 $url="http://$NODE_NAME"
-if (-not $env:ELASTICSEARCH_VERSION -contains "oss") {
+if (-not ($env:ELASTICSEARCH_VERSION -contains "oss")) {
   $url="https://elastic:$ELASTIC_PASSWORD@$NODE_NAME"
+}
+
+$cert_validation_flags = "--insecure"
+if ($NODE_NAME -eq "instance") {
+  $cert_validation_flags = "--cacert /usr/share/elasticsearch/config/certs/ca.pem --resolve ${NODE_NAME}:443:127.0.0.1"
 }
 
 Write-Output "$ESC[34;1mINFO:$ESC[0m Starting container $NODE_NAME $ESC[0m"
@@ -212,24 +234,26 @@ docker run `
   --ulimit nofile=65536:65536 `
   --ulimit memlock=-1:-1 `
   --detach="$d" `
-  --health-cmd="`"curl --silent --fail ${url}:9200/_cluster/health || exit 1`"" `
+  --health-cmd="`"curl ${cert_validation_flags} --fail ${url}:9200/_cluster/health || exit 1`"" `
   --health-interval=2s `
   --health-retries=20 `
   --health-timeout=2s `
   --rm docker.elastic.co/elasticsearch/"$($env:ELASTICSEARCH_VERSION)"
 
-
 if ($DETACH) {
-  while("$(docker inspect -f '{{.State.Health.Status}}' ${NODE_NAME})" -eq "starting") {
+  while((!$(container_running -Name $NODE_NAME)) -or ((container_running -Name $NODE_NAME) -and ("$(docker inspect -f '{{.State.Health.Status}}' ${NODE_NAME})" -eq "starting"))) {
     Start-Sleep 2;
     Write-Output ""
+    docker inspect -f '{{range .State.Health.Log}}{{.Output}}{{end}}' "$NODE_NAME"
     Write-Output "$ESC[34;1mINFO:$ESC[0m waiting for node $NODE_NAME to be up$ESC[0m"
   }
 
   # Always show the node getting started logs, this is very useful both on CI as well as while developing
-  docker logs "$NODE_NAME"
+  if (container_running -Name "$NODE_NAME") {
+    docker logs "$NODE_NAME"
+  }
 
-  if ("$(docker inspect -f '{{.State.Health.Status}}' ${NODE_NAME})" -ne "healthy") {
+  if (!$(container_running -Name $NODE_NAME) -or ("$(docker inspect -f '{{.State.Health.Status}}' ${NODE_NAME})" -ne "healthy")) {
     cleanup -Cleanup
     Write-Output ""
     Write-Output "$ESC[31;1mERROR:$ESC[0m Failed to start $($env:ELASTICSEARCH_VERSION) in detached mode beyond health checks$ESC[0m"
