579da122021c636a1dd7f2e43be6cf66df19d129
Russ Cam <russ.cam@elastic.co>
2020-02-28 17:37:31 +1000

    Update reqwest in api_generator

--- api_generator/src/rest_spec/mod.rs
+++ api_generator/src/rest_spec/mod.rs
@@ -61,10 +61,14 @@ pub fn download_specs(branch: &str, download_dir: &PathBuf) {
 }
 
 fn download_endpoints(spec: &GitHubSpec, download_dir: &PathBuf) {
-    let mut response = reqwest::get(&spec.url).unwrap();
-    let rest_api_specs: Vec<RestApiSpec> = response.json().unwrap();
+    let client = reqwest::blocking::ClientBuilder::new()
+        .user_agent(concat!("RustApiGenerator/", env!("CARGO_PKG_VERSION")))
+        .build()
+        .unwrap();
 
+    let response = client.get(&spec.url).send().unwrap();
+    let rest_api_specs: Vec<RestApiSpec> = response.json().unwrap();
     println!("Downloading {} specs from {}", spec.dir, spec.branch);
-    download_specs_to_dir(rest_api_specs.as_slice(), download_dir).unwrap();
+    download_specs_to_dir(client,rest_api_specs.as_slice(), download_dir).unwrap();
     println!("Done downloading {} specs from {}", spec.dir, spec.branch);
 }
