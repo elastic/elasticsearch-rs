3e759666f20b1fd40d78f9729e92aed0c932132c
Russ Cam <russ.cam@elastic.co>
2020-03-13 12:22:03 +1000

    address clippy warning

--- elasticsearch/src/http/transport.rs
+++ elasticsearch/src/http/transport.rs
@@ -148,27 +148,24 @@ impl TransportBuilder {
 
         #[cfg(any(feature = "native-tls", feature = "rustls-tls"))]
         {
-            if let Some(c) = &self.credentials {
-                match c {
-                    Credentials::Certificate(client_cert) => {
-                        client_builder = match client_cert {
-                            #[cfg(feature = "native-tls")]
-                            ClientCertificate::Pkcs12(b, p) => {
-                                let password = match p {
-                                    Some(pass) => pass.as_str(),
-                                    None => ""
-                                };
-                                let pkcs12 = reqwest::Identity::from_pkcs12_der(b, password)?;
-                                client_builder.identity(pkcs12)
-                            },
-                            #[cfg(feature = "rustls-tls")]
-                            ClientCertificate::Pem(b) => {
-                                let pem = reqwest::Identity::from_pem(b)?;
-                                client_builder.identity(pem)
-                            },
-                        }
+            if let Some(creds) = &self.credentials {
+                if let Credentials::Certificate(cert) = creds {
+                    client_builder = match cert {
+                        #[cfg(feature = "native-tls")]
+                        ClientCertificate::Pkcs12(b, p) => {
+                            let password = match p {
+                                Some(pass) => pass.as_str(),
+                                None => ""
+                            };
+                            let pkcs12 = reqwest::Identity::from_pkcs12_der(b, password)?;
+                            client_builder.identity(pkcs12)
+                        },
+                        #[cfg(feature = "rustls-tls")]
+                        ClientCertificate::Pem(b) => {
+                            let pem = reqwest::Identity::from_pem(b)?;
+                            client_builder.identity(pem)
+                        },
                     }
-                    _ => { },
                 }
             };
         }
