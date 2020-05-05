42ca7a21067fd970f598ab50b36722b77d8698b8
Russ Cam <russ.cam@forloop.co.uk>
2020-05-05 12:06:48 +1000

    Add license headers and NOTICE.txt

--- api_generator/src/api_generator/code_gen/url/url_builder.rs
+++ api_generator/src/api_generator/code_gen/url/url_builder.rs
@@ -1,3 +1,21 @@
+/*
+ * Licensed to Elasticsearch B.V. under one or more contributor
+ * license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright
+ * ownership. Elasticsearch B.V. licenses this file to you under
+ * the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *	http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 /* Some types from or based on types from elastic: https://github.com/elastic-rs/elastic
  *
  * Licensed under Apache 2.0: https://github.com/elastic-rs/elastic/blob/51298dd64278f34d2db911bd1a35eb757c336198/LICENSE-APACHE
@@ -14,7 +32,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 use crate::api_generator::code_gen::*;
 use crate::api_generator::{Path, Type, TypeKind};
 use quote::ToTokens;
