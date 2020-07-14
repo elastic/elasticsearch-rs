c6a0b9f26e15a2254c3a68bb29b1bdaf79568ed9
Russ Cam <russ.cam@forloop.co.uk>
2020-07-14 10:17:43 +1000

    Add copyright headers to examples
    (cherry picked from commit 817292fd9e4de998def06c27e17edaf71a88fb6a)

--- elasticsearch/examples/index_questions_answers/stack_overflow.rs
+++ elasticsearch/examples/index_questions_answers/stack_overflow.rs
@@ -1,12 +1,26 @@
-use chrono::prelude::*;
-use chrono::{DateTime, Utc};
+/*
+ * Licensed to Elasticsearch B.V. under one or more contributor
+ * license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright
+ * ownership. Elasticsearch B.V. licenses this file to you under
+ * the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *	http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+use chrono::{prelude::*, DateTime, Utc};
 use serde::{Deserialize, Serialize};
 use serde_json::Value;
-use std::collections::BTreeMap;
-use std::fs::File;
-use std::io::Read;
-use xml::reader::XmlEvent;
-use xml::EventReader;
+use std::{collections::BTreeMap, fs::File, io::Read};
+use xml::{reader::XmlEvent, EventReader};
 
 /// A Stack Overflow post
 #[derive(Debug, Serialize, Deserialize, PartialEq)]
