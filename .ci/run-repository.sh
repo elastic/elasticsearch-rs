bd306b9939898935b858e0a1e0d621694284eb49
Russ Cam <russ.cam@elastic.co>
2020-09-04 09:25:07 +1000

    Update CI to use cargo make (#136)
    This commit simplifies the CI test flow
    by
    
    - Using rust latest docker image as base image
    - Using cargo make to run the testing process
    
    pull the docker image before building to ensure that latest tag
    is the latest release.
    
    It appears that the -Z unstable-options --format json works
    on rust stable channel, despite rust documentation to the contrary.
    
    It also replaces the use of ES_TEST_SERVER with
    ELASTICSEARCH_URL, for consistency
    
    (cherry picked from commit 6c13b1d7be7080ed57726b17051a62bac5ec5a01)

--- .ci/run-repository.sh
+++ .ci/run-repository.sh
@@ -22,22 +22,22 @@ echo -e "\033[34;1mINFO:\033[0m RUST_TOOLCHAIN ${RUST_TOOLCHAIN}\033[0m"
 
 echo -e "\033[1m>>>>> Build [elastic/elasticsearch-rs container] >>>>>>>>>>>>>>>>>>>>>>>>>>>>>\033[0m"
 
+docker pull rust:"${RUST_TOOLCHAIN}"
+
 docker build --build-arg RUST_TOOLCHAIN="${RUST_TOOLCHAIN}" --file .ci/DockerFile --tag elastic/elasticsearch-rs .
 
 echo -e "\033[1m>>>>> Run [elastic/elasticsearch-rs container] >>>>>>>>>>>>>>>>>>>>>>>>>>>>>\033[0m"
 
 repo=$(realpath $(dirname $(realpath -s $0))/../)
 
-# ES_TEST_SERVER env var is needed for cargo test
 docker run \
   --network=${network_name} \
-  --env "ES_TEST_SERVER=${ELASTICSEARCH_URL}" \
+  --env "TEST_SUITE=${TEST_SUITE}" \
+  --env "STACK_VERSION=${STACK_VERSION}" \
+  --env "ELASTICSEARCH_URL=${ELASTICSEARCH_URL}" \
+  --env "CI=true" \
   --name test-runner \
   --volume ${repo}/test_results:/usr/src/elasticsearch-rs/test_results \
   --rm \
   elastic/elasticsearch-rs \
-  /bin/bash -c \
-  "cargo run -p yaml_test_runner -- -u \"${ELASTICSEARCH_URL}\"; \\
-   mkdir -p test_results; \\
-   cargo test -p yaml_test_runner -- --test-threads=1 -Z unstable-options --format json | tee test_results/results.json; \\
-   cat test_results/results.json | cargo2junit > test_results/cargo-junit.xml"
+  /bin/bash -c "cargo make test-yaml"
