312d6aa4cf24502c41255b91c31f40055ad79d22
Joel Höner <joel@elastic.co>
2022-09-06 22:16:30 +0200

    yaml_test_runner: update `reqwest` dependency

--- yaml_test_runner/src/main.rs
+++ yaml_test_runner/src/main.rs
@@ -127,7 +127,7 @@ fn main() -> anyhow::Result<()> {
 fn branch_suite_and_version_from_elasticsearch(
     url: &str,
 ) -> Result<(String, TestSuite, String, semver::Version), failure::Error> {
-    let client = reqwest::ClientBuilder::new()
+    let client = reqwest::blocking::ClientBuilder::new()
         .danger_accept_invalid_certs(true)
         .build()?;
 
@@ -136,7 +136,7 @@ fn branch_suite_and_version_from_elasticsearch(
         Ok(ref s) if s == "free" => TestSuite::Free,
         _ => TestSuite::XPack,
     };
-    let mut response = client.get(url).send()?;
+    let response = client.get(url).send()?;
     let json: Value = response.json()?;
     let branch = json["version"]["build_hash"].as_str().unwrap().to_string();
 
